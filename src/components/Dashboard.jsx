2bb901e3a75d78d6bb9c7adb2f844116e169ff08
SamH135 <samuelheidler@gmail.com>
2024-07-13 14:30:38 -0500

    removed testing features

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -14,7 +14,6 @@ const Dashboard = () => {
         <h4>Client Management System</h4>
         <ul>
           <li><Logout /></li>
-          <li><TestSessionExpireButton /></li>
         </ul>
       </nav>
 
